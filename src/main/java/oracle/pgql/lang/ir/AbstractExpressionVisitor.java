676933f3c35e320cb1a3370b995ab7fd342f10ef
Oskar van Rest <oskar.van.rest@oracle.com>
2016-03-18 17:01:23 -0700

    brough back ExpressionVisitor interface

--- src/main/java/oracle/pgql/lang/ir/AbstractExpressionVisitor.java
+++ src/main/java/oracle/pgql/lang/ir/AbstractExpressionVisitor.java
@@ -1,8 +1,8 @@
+/**
+ * Copyright (C) 2013 - 2016 Oracle and/or its affiliates. All rights reserved.
+ */
 package oracle.pgql.lang.ir;
 
-import oracle.pgql.lang.ir.QueryExpression.ConstNull;
-import oracle.pgql.lang.ir.QueryExpression.PropAccess;
-import oracle.pgql.lang.ir.QueryExpression.VarRef;
 import oracle.pgql.lang.ir.QueryExpression.Aggregation.AggrAvg;
 import oracle.pgql.lang.ir.QueryExpression.Aggregation.AggrCount;
 import oracle.pgql.lang.ir.QueryExpression.Aggregation.AggrMax;
@@ -15,6 +15,7 @@ import oracle.pgql.lang.ir.QueryExpression.ArithmeticExpression.Mod;
 import oracle.pgql.lang.ir.QueryExpression.ArithmeticExpression.Mul;
 import oracle.pgql.lang.ir.QueryExpression.ArithmeticExpression.Sub;
 import oracle.pgql.lang.ir.QueryExpression.ArithmeticExpression.UMin;
+import oracle.pgql.lang.ir.QueryExpression.ConstNull;
 import oracle.pgql.lang.ir.QueryExpression.Constant.ConstBoolean;
 import oracle.pgql.lang.ir.QueryExpression.Constant.ConstDecimal;
 import oracle.pgql.lang.ir.QueryExpression.Constant.ConstInteger;
@@ -30,84 +31,86 @@ import oracle.pgql.lang.ir.QueryExpression.Function.VertexLabels;
 import oracle.pgql.lang.ir.QueryExpression.LogicalExpression.And;
 import oracle.pgql.lang.ir.QueryExpression.LogicalExpression.Not;
 import oracle.pgql.lang.ir.QueryExpression.LogicalExpression.Or;
+import oracle.pgql.lang.ir.QueryExpression.PropAccess;
 import oracle.pgql.lang.ir.QueryExpression.RelationalExpression.Equal;
 import oracle.pgql.lang.ir.QueryExpression.RelationalExpression.Greater;
 import oracle.pgql.lang.ir.QueryExpression.RelationalExpression.GreaterEqual;
 import oracle.pgql.lang.ir.QueryExpression.RelationalExpression.Less;
 import oracle.pgql.lang.ir.QueryExpression.RelationalExpression.LessEqual;
 import oracle.pgql.lang.ir.QueryExpression.RelationalExpression.NotEqual;
+import oracle.pgql.lang.ir.QueryExpression.VarRef;
 
-public interface ExpressionVisitor {
-	
-	  public void visit(ConstInteger constInteger);
+public abstract class AbstractExpressionVisitor implements ExpressionVisitor {
 
-	  public void visit(ConstDecimal constDecimal);
+  public void visit(ConstInteger constInteger) { }
 
-	  public void visit(ConstString constString);
+  public void visit(ConstDecimal constDecimal) { }
 
-	  public void visit(ConstBoolean constBoolean);
+  public void visit(ConstString constString) { }
 
-	  public void visit(ConstNull constantNull);
+  public void visit(ConstBoolean constBoolean) { }
 
-	  public void visit(Sub sub);
+  public void visit(ConstNull constantNull) { }
 
-	  public void visit(Add add);
+  public void visit(Sub sub) { }
 
-	  public void visit(Mul mul);
+  public void visit(Add add) { }
 
-	  public void visit(Div div);
+  public void visit(Mul mul) { }
 
-	  public void visit(Mod mod);
+  public void visit(Div div) { }
 
-	  public void visit(UMin uMin);
+  public void visit(Mod mod) { }
 
-	  public void visit(And and);
+  public void visit(UMin uMin) { }
 
-	  public void visit(Or or);
+  public void visit(And and) { }
 
-	  public void visit(Not not);
+  public void visit(Or or) { }
 
-	  public void visit(Equal equal);
+  public void visit(Not not) { }
 
-	  public void visit(NotEqual notEqual);
+  public void visit(Equal equal) { }
 
-	  public void visit(Greater greater);
+  public void visit(NotEqual notEqual) { }
 
-	  public void visit(GreaterEqual greaterEqual);
+  public void visit(Greater greater) { }
 
-	  public void visit(Less less);
+  public void visit(GreaterEqual greaterEqual) { }
 
-	  public void visit(LessEqual lessEqual);
+  public void visit(Less less) { }
 
-	  public void visit(AggrCount aggrCount);
+  public void visit(LessEqual lessEqual) { }
 
-	  public void visit(AggrMin aggrMin);
+  public void visit(AggrCount aggrCount) { }
 
-	  public void visit(AggrMax aggrMax);
+  public void visit(AggrMin aggrMin) { }
 
-	  public void visit(AggrSum aggrSum);
+  public void visit(AggrMax aggrMax) { }
 
-	  public void visit(AggrAvg aggrAvg);
+  public void visit(AggrSum aggrSum) { }
 
-	  public void visit(VarRef varRef);
+  public void visit(AggrAvg aggrAvg) { }
 
-	  public void visit(Star star);
+  public void visit(VarRef varRef) { }
 
-	  public void visit(Regex regex);
+  public void visit(Star star) { }
 
-	  public void visit(Id id);
+  public void visit(Regex regex) { }
 
-	  public void visit(PropAccess propAccess);
+  public void visit(Id id) { }
 
-	  public void visit(HasProp hasProp);
+  public void visit(PropAccess propAccess) { }
 
-	  public void visit(HasLabel hasLabel);
+  public void visit(HasProp hasProp) { }
 
-	  public void visit(VertexLabels vertexLabels);
+  public void visit(HasLabel hasLabel) { }
 
-	  public void visit(InDegree inDegree);
+  public void visit(VertexLabels vertexLabels) { }
 
-	  public void visit(OutDegree outDegree);
+  public void visit(InDegree inDegree) { }
 
-	  public void visit(EdgeLabel edgeLabel);
+  public void visit(OutDegree outDegree) { }
+
+  public void visit(EdgeLabel edgeLabel) { }
 }
