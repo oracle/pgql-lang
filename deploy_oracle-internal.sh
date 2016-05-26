c288fa3ca2aa4a63461cba43fba2bd7d20bf56db
Oskar van Rest <oskar.van.rest@oracle.com>
2016-05-25 18:53:00 -0700

    host spoofax locally until beta2 (no uber jar available for beta1)

--- deploy_oracle-internal.sh
+++ deploy_oracle-internal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION=2016.05.23-1
+VERSION=2016.05.25-2
 GROUP=oracle.pgx
 REPO=http://artifactory-slc.oraclecorp.com/artifactory/simple/labs-pgx-release-local
 REPO_ID=labs-pgx-release-local
