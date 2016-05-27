1593ecd83de070eacfbcf189bf9a74431b80267f
Oskar van Rest <oskar.van.rest@oracle.com>
2016-05-26 17:40:46 -0700

    name binding for path queries

--- deploy_oracle-internal.sh
+++ deploy_oracle-internal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION=2016.05.26-4
+VERSION=2016.05.26-5
 GROUP=oracle.pgx
 REPO=http://artifactory-slc.oraclecorp.com/artifactory/simple/labs-pgx-release-local
 REPO_ID=labs-pgx-release-local
