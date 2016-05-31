5b67eff679fa6b787732837950c414b6a51e129f
Oskar van Rest <oskar.van.rest@oracle.com>
2016-05-31 14:50:41 -0700

    start using SNAPSHOT in version such that we don't need to choose a new version each time

--- deploy_oracle-internal.sh
+++ deploy_oracle-internal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION=2016.05.31
+VERSION=0.9-SNAPSHOT
 GROUP=oracle.pgx
 REPO=http://artifactory-slc.oraclecorp.com/artifactory/simple/labs-pgx-release-local
 REPO_ID=labs-pgx-release-local
