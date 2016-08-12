c6e39db31811d6894e27291e55237b00cbaf846c
Oskar van Rest <oskar.van.rest@oracle.com>
2016-08-11 23:13:46 -0700

    url fix

--- GenerateMenuFromMarkdown.java
+++ GenerateMenuFromMarkdown.java
@@ -55,7 +55,7 @@ public class GenerateMenuFromMarkdown {
       level = newLevel;
 
       result += indent.substring(2) + "- title: " + title + "\n";
-      result += indent + "url: " + url_prefix + title.toLowerCase().replace(" ", "-").replace("*", "") + "\n";
+      result += indent + "url: " + url_prefix + title.toLowerCase().replace(" ", "-").replace("*", "").replace("/", "") + "\n";
       result += indent + "output: web\n";
     }
 
