75c56e78cc846ab36a5dbe9b24a0bc1fe41538b6
Oskar van Rest <oskar.van.rest@oracle.com>
2016-08-21 21:16:00 -0700

    -XX:MaxPermSize option no longer needed in Java 8

--- example/run.sh
+++ example/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+export MAVEN_OPTS="-Xms512m -Xmx1024m -Xss16m"
+
 mvn clean package exec:java -Dexec.mainClass="oracle.pgql.lang.example.Main" -Dexec.cleanupDaemonThreads=false
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-export MAVEN_OPTS="-Xms512m -Xmx1024m -Xss16m -XX:MaxPermSize=512m"
+export MAVEN_OPTS="-Xms512m -Xmx1024m -Xss16m"
 
 cd pgql-spoofax/; mvn clean install; cd ../
 
