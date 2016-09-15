0040f8327006124b5ef85b098a2a7a3a3e7ee4be
Oskar van Rest <oskar.van.rest@oracle.com>
2016-09-14 18:29:04 -0700

    make it build on OSX

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 set -e
 export MAVEN_OPTS="-Xms512m -Xmx1024m -Xss16m"
 
-cd pgql-spoofax/; mvn clean install; cd ../
+cd pgql-spoofax/; mvn clean package; cd ../
 
 cd graph-query-ir/; mvn clean install; cd ../
 
 cd pgql-lang/
-mkdir -p src/main/resources/pgql-spoofax-binaries
-cp ../pgql-spoofax/target/pgqllang-1.0.3-SNAPSHOT.spoofax-language -d src/main/resources/pgql-1.0.spoofax-language
+mkdir -p src/main/resources/
+cp ../pgql-spoofax/target/pgql-lang-1.0.3-SNAPSHOT.spoofax-language src/main/resources/pgql-1.0.spoofax-language
 mvn clean install
+cd ../
