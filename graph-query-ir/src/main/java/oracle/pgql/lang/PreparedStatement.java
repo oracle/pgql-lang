9b64fc0b481813e2eab6924172636a0e6247e654
Wilco van Leeuwen <wilco.van.leeuwen@oracle.com>
2017-04-25 10:12:19 -0700

    Support for temporal types with offset in prepared statements

--- graph-query-ir/src/main/java/oracle/pgql/lang/PreparedStatement.java
+++ graph-query-ir/src/main/java/oracle/pgql/lang/PreparedStatement.java
@@ -6,6 +6,8 @@ package oracle.pgql.lang;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
 
 /**
  * An object that represents a precompiled PGQL statement.
@@ -112,6 +114,26 @@ public interface PreparedStatement extends AutoCloseable {
    *          the parameter value
    */
   void setTimestamp(int parameterIndex, LocalDateTime x) throws PgqlException;
+
+  /**
+   * Sets the designated parameter to the given Java OffsetTime value.
+   *
+   * @param parameterIndex
+   *          the first parameter is 1, the second is 2, ...
+   * @param x
+   *          the parameter value
+   */
+  void setTimeWithTimezone(int parameterIndex, OffsetTime x) throws PgqlException;
+
+  /**
+   * Sets the designated parameter to the given Java OffsetDateTime value.
+   *
+   * @param parameterIndex
+   *          the first parameter is 1, the second is 2, ...
+   * @param x
+   *          the parameter value
+   */
+  void setTimestampWithTimezone(int parameterIndex, OffsetDateTime x) throws PgqlException;
 
   /**
    * Releases this statement's resources. Calling the method close on a Statement object that is already closed has no
