07840badc41018a3efbfac9696c2fdba34bae025
Wilco van Leeuwen <wilco.van.leeuwen@oracle.com>
2017-06-19 10:08:54 -0700

    Separate spatial functions in preparedStatement

---
+++ graph-query-ir/src/main/java/oracle/pgql/lang/spatial/STPreparedStatement.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2013 - 2017 Oracle and/or its affiliates. All rights reserved.
+ */
+package oracle.pgql.lang.spatial;
+
+import oracle.pgql.lang.PgqlException;
+
+public interface STPreparedStatement {
+
+  /**
+   * Sets the designated parameter to the given Point2D value.
+   *
+   * @param parameterIndex
+   *          the first parameter is 1, the second is 2, ...
+   * @param x
+   *          the parameter value
+   */
+  void setPoint2D(int parameterIndex, Point2D x) throws PgqlException;
+}
