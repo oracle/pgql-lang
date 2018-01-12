50ed3e3acd83900c533066c3c24d33bc7a5dce58
Oskar van Rest <oskar.van.rest@oracle.com>
2018-01-12 14:25:24 -0800

    make query IR mutable

--- graph-query-ir/src/main/java/oracle/pgql/lang/ir/ExpAsVar.java
+++ graph-query-ir/src/main/java/oracle/pgql/lang/ir/ExpAsVar.java
@@ -7,23 +7,30 @@ import static oracle.pgql.lang.ir.PgqlUtils.printPgqlString;
 
 public class ExpAsVar extends QueryVariable {
 
-  private final QueryExpression exp;
+  private QueryExpression exp;
 
   /**
-   * @param exp       an expression
-   * @param name      the name with which the the element can be referred to in the result set
-   * @param anonymous false if the name was provided via the query (i.e. exp AS name), true if the name
-   *                  was not provided via the query (i.e. exp) but via some other mechanism
+   * @param exp
+   *          an expression
+   * @param name
+   *          the name with which the the element can be referred to in the result set
+   * @param anonymous
+   *          false if the name was provided via the query (i.e. exp AS name), true if the name was not provided via the
+   *          query (i.e. exp) but via some other mechanism
    */
   public ExpAsVar(QueryExpression exp, String name, boolean anonymous) {
     super(name, anonymous);
     this.exp = exp;
   }
-  
+
   public QueryExpression getExp() {
     return exp;
   }
-  
+
+  public void setExp(QueryExpression exp) {
+    this.exp = exp;
+  }
+
   @Override
   public String toString() {
     return printPgqlString(this);
