9b09e03de96d48905c6fa3b0fe2e14b42da34c4d
Oskar van Rest <oskar.van.rest@oracle.com>
2018-07-16 14:04:28 -0700

    error messages for invalid datetimes

---
+++ pgql-spoofax/src/main/strategies/pgqllang/strategies/is_valid_datetime_0_0.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2013 - 2018 Oracle and/or its affiliates. All rights reserved.
+ */
+package pgqllang.strategies;
+
+import static java.time.temporal.ChronoField.HOUR_OF_DAY;
+import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
+import static java.time.temporal.ChronoField.NANO_OF_SECOND;
+import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.chrono.IsoChronology;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
+import org.spoofax.interpreter.terms.IStrategoAppl;
+import org.spoofax.interpreter.terms.IStrategoString;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.strategoxt.lang.Context;
+import org.strategoxt.lang.Strategy;
+
+public class is_valid_datetime_0_0 extends Strategy {
+
+  public static is_valid_datetime_0_0 instance = new is_valid_datetime_0_0();
+
+  public static final DateTimeFormatter SQL_DATE = DateTimeFormatter.ISO_LOCAL_DATE;
+
+  /**
+   * Differences with DateTimeFormatter.ISO_LOCAL_TIME: - seconds are optional in ISO but not in SQL
+   */
+  public static final DateTimeFormatter SQL_TIME = new DateTimeFormatterBuilder() //
+      .appendValue(HOUR_OF_DAY, 2) //
+      .appendLiteral(':') //
+      .appendValue(MINUTE_OF_HOUR, 2) //
+      .appendLiteral(':') //
+      .appendValue(SECOND_OF_MINUTE, 2) //
+      .optionalStart() //
+      .appendFraction(NANO_OF_SECOND, 0, 9, true).toFormatter().withResolverStyle(ResolverStyle.STRICT);
+
+  /**
+   * Differences with DateTimeFormatter.ISO_LOCAL_DATE_TIME: - seconds are optional in ISO but not in SQL - date and
+   * time are separated by 'T' in ISO but by ' ' in SQL
+   */
+  public static final DateTimeFormatter SQL_TIMESTAMP = new DateTimeFormatterBuilder() //
+      .append(SQL_DATE) //
+      .appendLiteral(' ') //
+      .append(SQL_TIME).toFormatter().withResolverStyle(ResolverStyle.STRICT).withChronology(IsoChronology.INSTANCE);
+
+  /**
+   * Differences with DateTimeFormatter.ISO_OFFSET_TIME: - seconds are optional in ISO but not in SQL - a zero offset in
+   * ISO can optionally be denoted by appending 'Z' at the end, but this is not allowed in SQL
+   */
+  public static final DateTimeFormatter SQL_TIME_WITH_TIMEZONE = new DateTimeFormatterBuilder() //
+      .append(SQL_TIME) //
+      .appendOffset("+HH:MM", "").toFormatter();
+
+  /**
+   * Differences with DateTimeFormatter.ISO_OFFSET_DATE_TIME: - seconds are optional in ISO but not in SQL - date and
+   * time are separated by 'T' in ISO but by ' ' in SQL - a zero offset in ISO can optionally be denoted by appending
+   * 'Z' at the end, but this is not allowed in SQL
+   */
+  public static final DateTimeFormatter SQL_TIMESTAMP_WITH_TIMEZONE = new DateTimeFormatterBuilder() //
+      .append(SQL_TIMESTAMP) //
+      .appendOffset("+HH:MM", "").toFormatter();
+
+  @Override
+  public IStrategoTerm invoke(Context context, IStrategoTerm current) {
+
+    IStrategoAppl appl = (IStrategoAppl) current;
+    String s = ((IStrategoString) appl.getSubterm(0)).stringValue();
+
+    switch (appl.getConstructor().getName()) {
+      case "Date":
+        try {
+          LocalDate.parse(s, SQL_DATE);
+          return current;
+        } catch (DateTimeParseException e) {
+          return null;
+        }
+      case "Time":
+        try {
+          LocalTime.parse(s, SQL_TIME);
+          return current;
+        } catch (DateTimeParseException e) {
+          try {
+            OffsetTime.parse(s, SQL_TIME_WITH_TIMEZONE);
+            return current;
+          } catch (DateTimeParseException e2) {
+            return null;
+          }
+        }
+      case "Timestamp":
+        try {
+          LocalDateTime.parse(s, SQL_TIMESTAMP);
+          return current;
+        } catch (DateTimeParseException e) {
+          try {
+            OffsetDateTime.parse(s, SQL_TIMESTAMP_WITH_TIMEZONE);
+            return current;
+          } catch (DateTimeParseException e2) {
+            return null;
+          }
+        }
+      default:
+        return null;
+    }
+  }
+}
