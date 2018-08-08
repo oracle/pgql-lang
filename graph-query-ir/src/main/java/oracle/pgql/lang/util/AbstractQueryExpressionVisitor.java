82781ca8a602c3fb5fcbc026f3d1d920901a2319
Oskar van Rest <oskar.van.rest@oracle.com>
2018-08-07 18:38:03 -0700

    add support for NOT IN and fix wrong bind variable count bug

--- graph-query-ir/src/main/java/oracle/pgql/lang/util/AbstractQueryExpressionVisitor.java
+++ graph-query-ir/src/main/java/oracle/pgql/lang/util/AbstractQueryExpressionVisitor.java
@@ -237,6 +237,7 @@ public abstract class AbstractQueryExpressionVisitor implements QueryExpressionV
   @Override
   public void visit(InPredicate inPredicate) {
     inPredicate.getExp().accept(this);
+    inPredicate.getInValueList().accept(this);
   }
 
   @Override
