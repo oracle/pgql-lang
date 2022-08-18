b4a376a183548c8c5e01897bcc68bf5ba25e594f
Oskar van Rest <oskar.van.rest@oracle.com>
2022-08-18 15:45:58 -0700

    deploy to stage repo unless version is a release version

--- deploy.sh
+++ deploy.sh
@@ -20,12 +20,24 @@ else
   exit
 fi
 
+STAGE=false
+if [[ "$1" =~ ^[0-9.]+$ ]]
+then
+  echo "Deploying to release repo"
+else
+  echo "Deploying to staging repo"
+  STAGE=true
+fi
+
 VERSION_A="0.0.0-SNAPSHOT"
 VERSION_B="$1"
 
 for f in $FILES
 do
   sed -i.bak "s/$VERSION_A/$VERSION_B/g" $f
+  if $STAGE
+  then sed -i.bak "s/release-local/stage-local/g" $f
+  fi
 done
 
 cd graph-query-ir/; mvn deploy; cd ../
@@ -34,4 +46,7 @@ cd pgql-lang/; mvn deploy; cd ../
 for f in $FILES
 do
   sed -i.bak "s/$VERSION_B/$VERSION_A/g" $f
+  if $STAGE
+  then sed -i.bak "s/stage-local/release-local/g" $f
+  fi
 done
