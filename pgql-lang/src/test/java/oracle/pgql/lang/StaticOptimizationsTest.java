24eab3decf5512e59510e5674d3c4f2e64f1db1e
Oskar van Rest <oskar.van.rest@oracle.com>
2023-04-25 15:42:18 -0700

    fix typos in comments

--- pgql-lang/src/test/java/oracle/pgql/lang/StaticOptimizationsTest.java
+++ pgql-lang/src/test/java/oracle/pgql/lang/StaticOptimizationsTest.java
@@ -265,7 +265,7 @@ public class StaticOptimizationsTest extends AbstractPgqlTest {
 
   @Test
   public void testReachesOptimization() throws Exception {
-    // test that ANY translates to REACHES as long as vertex and edge variables in the parenthesized pattern do not have group degree of reference
+    // test that ANY translates to REACHES as long as vertex and edge variables in the quantified pattern do not have group degree of reference
 
     GraphQuery graphQuery = pgql.parse("SELECT 1 FROM MATCH ANY () ->* ()").getGraphQuery();
     QueryPath path = (QueryPath) graphQuery.getGraphPattern().getConnections().iterator().next();
