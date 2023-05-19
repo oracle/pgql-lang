772c5a9a37963725431bdd6b5ba49a2516c0e8a0
Oskar van Rest <oskar.van.rest@oracle.com>
2023-05-19 13:37:46 -0700

    add PGQL 2.0 navigation menus

--- generate-menus-and-hyperlinks.sh
+++ generate-menus-and-hyperlinks.sh
@@ -8,6 +8,7 @@ java GenerateMenuFromMarkdown 1.2
 java GenerateMenuFromMarkdown 1.3
 java GenerateMenuFromMarkdown 1.4
 java GenerateMenuFromMarkdown 1.5
+java GenerateMenuFromMarkdown 2.0
 
 # regenerate anchor points and links for grammar:
 javac GeneratePostProcessingRules.java
