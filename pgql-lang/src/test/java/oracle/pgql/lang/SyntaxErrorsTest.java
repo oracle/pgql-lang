4b7896adb21e1a3b8736ea32a07b8021523cb32b
Oskar van Rest <oskar.van.rest@oracle.com>
2023-07-26 10:37:56 -0700

    remove legacy SPARQL-like ASC/DESC ordering

--- pgql-lang/src/test/java/oracle/pgql/lang/SyntaxErrorsTest.java
+++ pgql-lang/src/test/java/oracle/pgql/lang/SyntaxErrorsTest.java
@@ -37,15 +37,6 @@ public class SyntaxErrorsTest extends AbstractPgqlTest {
   }
 
   @Test
-  public void testSparqlLikeAscDesc() throws Exception {
-    PgqlResult result = pgql.parse("SELECT n.prop MATCH (n) ORDER BY ASC(n.prop1), DESC(n.prop2)");
-    assertFalse(result.isQueryValid());
-    assertTrue(result.getErrorMessages().contains("Use [n.prop1 ASC] instead of [ASC(n.prop1)]"));
-    assertTrue(result.getErrorMessages().contains("Use [n.prop2 DESC] instead of [DESC(n.prop2)]"));
-    assertFalse(result.getGraphQuery() == null);
-  }
-
-  @Test
   public void testSelectStarGroupVariable() throws Exception {
     PgqlResult result = pgql.parse("SELECT * MATCH SHORTEST ( (n) -[e:lbl]->+ (m) )");
     assertFalse(result.isQueryValid());
